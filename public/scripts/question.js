1309646c5e62ecd489357164dd2401698a5924e1
ASN <eaaassn@students.eaaa.dk>
2022-05-06 11:57:27 +0200

    import scripts

--- public/scripts/question.js
+++ public/scripts/question.js
@@ -1,7 +1,6 @@
 function page_load() {
-
+    document.cookie = "question_counter = 0; answers = []; "
 }
-
 
 function option_0_button_click() {
     console.log("clicked 0");
@@ -9,4 +8,5 @@ function option_0_button_click() {
 
 function option_1_button_click() {
     console.log("clicked 1");
+    test();
 }
