1e1ef10c05006e482daa452f935f639cd40a0005
ASN <eaaassn@students.eaaa.dk>
2022-05-08 10:18:58 +0200

    moved update_id func

--- public/scripts/answer.js
+++ public/scripts/answer.js
@@ -1,3 +1,21 @@
 function page_load() {
     check_question_id();    
 }
+
+//Counts question in localStorage
+function update_question_id(){
+    let question_id = JSON.parse(localStorage.getItem("localQuestionId"));
+    localStorage.setItem('localQuestionId', ++question_id);
+}
+
+//Controls button clicks, redirects to href value from button, after question 10 directs to result.
+function next_button_click() {
+    let question_id = localStorage.getItem('localQuestionId');
+    update_question_id();
+    if(question_id<10){
+        location.href = '/question.html';
+    }
+    else {
+        location.href = '/result.html'
+    } 
+}
--- public/scripts/question.js
+++ public/scripts/question.js
@@ -2,12 +2,6 @@ function page_load() {
     check_question_id();
 }
 
-//Counts question in localStorage
-function update_question_id(){
-    let question_id = JSON.parse(localStorage.getItem("localQuestionId"));
-    localStorage.setItem('localQuestionId', ++question_id);
-}
-
 function update_answer(option_chosen) {
     if(localStorage.getItem('localAnswers') != null){
         let local_answers = JSON.parse(localStorage.getItem('localAnswers'));
@@ -21,7 +15,6 @@ function update_answer(option_chosen) {
 }
 
 function option_button_click(value) {
-    update_question_id();
     update_answer(value);
     location.href = '/answer.html';
 }
