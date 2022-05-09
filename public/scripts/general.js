cfb37b98afe3f7bcb97554a0305ecec6ac7ae8eb
Mads Lajer <eaamalr@students.eaaa.dk>
2022-05-09 09:41:10 +0200

    Removed next_button function from general

--- public/scripts/general.js
+++ public/scripts/general.js
@@ -7,14 +7,3 @@ function check_question_id() {
     console.log("localQuestionId: " + localStorage.getItem('localQuestionId'));
     console.log("localAnswers: " + localStorage.getItem('localAnswers'));
 }
-
-//Controls button clicks, redirects to href value from button, after question 10 directs to result.
-function next_button_click() {
-    let question_id = localStorage.getItem('localQuestionId');
-    if(question_id<10){
-        location.href = '/question.html';
-    }
-    else {
-        location.href = '/result.html'
-    } 
-}
