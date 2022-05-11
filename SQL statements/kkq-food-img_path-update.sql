82d6ad971ee49d15c274ffd721985c810127e225
Elykke <Lordlykkegaard@gmail.com>
2022-05-11 14:42:12 +0200

    update food icon for db

---
+++ SQL statements/kkq-food-img_path-update.sql
@@ -0,0 +1,79 @@
+UPDATE food
+    SET img_path = '/images/Icon_1_0.png'
+    WHERE food_id = 61;
+
+UPDATE food
+    SET img_path = '/images/Icon_1_1.png'
+    WHERE food_id = 49;
+
+UPDATE food
+    SET img_path = '/images/Icon_2_0.png'
+    WHERE food_id = 322;
+
+UPDATE food
+    SET img_path = '/images/Icon_2_1.png'
+    WHERE food_id = 286;
+
+UPDATE food
+    SET img_path = '/images/Icon_3_0.png'
+    WHERE food_id = 263;
+
+UPDATE food
+    SET img_path = '/images/Icon_3_1.png'
+    WHERE food_id = 265;  
+
+UPDATE food
+    SET img_path = '/images/Icon_4_0.png'
+    WHERE food_id = 420;      
+    
+UPDATE food
+    SET img_path = '/images/Icon_4_1.png'
+    WHERE food_id = 393;     
+
+UPDATE food
+    SET img_path = '/images/Icon_5_0.png'
+    WHERE food_id = 379; 
+
+UPDATE food
+    SET img_path = '/images/Icon_5_1.png'
+    WHERE food_id = 387; 
+
+UPDATE food
+    SET img_path = '/images/Icon_6_0.png'
+    WHERE food_id = 121;  
+
+UPDATE food
+    SET img_path = '/images/Icon_6_1.png'
+    WHERE food_id = 119;  
+
+UPDATE food
+    SET img_path = '/images/Icon_7_0.png'
+    WHERE food_id = 72;
+
+UPDATE food
+    SET img_path = '/images/Icon_7_1.png'
+    WHERE food_id = 91;   
+
+UPDATE food
+    SET img_path = '/images/Icon_8_0.png'
+    WHERE food_id = 403;    
+
+UPDATE food
+    SET img_path = '/images/Icon_8_1.png'
+    WHERE food_id = 401;  
+
+UPDATE food
+    SET img_path = '/images/Icon_9_0.png'
+    WHERE food_id = 457;               
+
+UPDATE food
+    SET img_path = '/images/Icon_9_1.png'
+    WHERE food_id = 495;     
+
+UPDATE food
+    SET img_path = '/images/Icon_10_0.png'
+    WHERE food_id = 87; 
+
+UPDATE food
+    SET img_path = '/images/Icon_10_1.png'
+    WHERE food_id = 292;            
