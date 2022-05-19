337720a6289fc03e0fde4a597758f7182afc89d9
ASN <eaaassn@students.eaaa.dk>
2022-05-19 21:37:17 +0200

    dynamic alignment for tooltip + desaturation on hover for breakdown

--- public/scripts/answer.js
+++ public/scripts/answer.js
@@ -1,16 +1,21 @@
 function page_load() {
+    // Simpel log check for localQuestionCounter (ligger i general.js)
     check_question_id();
-    get_question(localStorage.getItem('localQuestionId'));
+
+    // funktion der henter data fra api kald og loader data elementer
     get_answer();
+
+    // sætter progress counter i bunden af siden
     d3.select('#progress').text(`${localStorage.getItem('localQuestionId')}/10`);
 }
 
-//Counts question in localStorage
+// funktion der opdaterer question_id i localstorage
 function update_question_id() {
     let question_id = JSON.parse(localStorage.getItem("localQuestionId"));
     localStorage.setItem('localQuestionId', ++question_id);
 }
 
+// funktion kaldet når next knappen bliver klikket på og redirecter udfra question_id i localStorage
 function next_button_click() {
     let question_id = localStorage.getItem('localQuestionId');
     update_question_id();
@@ -22,15 +27,10 @@ function next_button_click() {
     }
 }
 
-function get_question(id) {
-    d3.json("/api/question/" + id, {
-        method: "GET"
-    }).then(function (response) {
-        const data = response.data; // Hent data ud af response
-    })
-}
-
+// funktion der henter data fra api kald og loader data elementer
 function get_answer() {
+
+    //
     fetch('/api/questionbreakdown/' + localStorage.getItem("localQuestionId"))
         .then(response => response.json())
         .then(data => {
@@ -61,14 +61,12 @@ function get_answer() {
 
             if (answers[currentQuestion - 1] == correctAnswer) {
                 d3.select('#quiz_answer').text(`Du svarede rigtigt! ${data.data[correctAnswer].name.split(",")[0]} udleder MINDRE co2e end ${data.data[1 - correctAnswer].name.split(",")[0]}.`);
-                console.log(`#quiz_answer_${correctAnswer}_checkmark`);
                 d3.select(`#quiz_answer_${correctAnswer}_checkmark`)
                     .style("background-color", "#90da50")
                     .append("img")
                     .attr("src", "images/checkmark.png");
             } else {
                 d3.select('#quiz_answer').text(`Du svarede forkert! ${data.data[1 - correctAnswer].name.split(",")[0]} udleder MERE co2e end ${data.data[correctAnswer].name.split(",")[0]}.`);
-                console.log(`#quiz_answer_${1 - correctAnswer}_checkmark`);
                 d3.select(`#quiz_answer_${1 - correctAnswer}_checkmark`)
                     .style("background-color", "#444444")
                     .append("img")
@@ -96,6 +94,7 @@ function get_answer() {
 
             const svg = d3.select("#emission_breakdown")
                 .append("svg")
+                .attr("id", "emission_breakdown_svg")
                 .attr("width", "100%")
                 .attr("height", "100%");
 
@@ -150,7 +149,6 @@ function get_answer() {
                 method: "GET"
             }).then(function (response) {
                 const emissionCategories = response.data;
-                console.log(emissionCategories.length);
                 emissionCategories.forEach(emission_category => {
 
                     let barId = "";
@@ -176,32 +174,19 @@ function get_answer() {
                         default:
                             break;
                     }
-                    console.log(emission_category.category.toLowerCase());
                     d3.selectAll("#" + barId)
                         .on("mouseover", function () {
                             let iconElement = document.getElementById(emission_category.danish_category.toLowerCase() + "_icon").getBoundingClientRect();
-
-                            let xPosition = 0;
-
-                            if (parseInt(emission_category.category_id) < 4) {
-                                xPosition = iconElement.x + iconElement.width;
-                            } else {
-                                xPosition = iconElement.x - (45 * window.innerWidth / 100) - 5;
-                            }
-
-                            let yPosition = document.getElementById("emission_breakdown").getBoundingClientRect().top;
-
-                            d3.select("#tooltip")
-                                .style("left", xPosition + "px")
-                                .style("top", yPosition + "px")
-                                .select("h2")
-                                .text(`${emission_category.danish_category}`);
 
                             d3.select("#tooltip_answer_0")
                                 .text(`${data.data[0].name.split(",")[0]}: ${parseFloat(data.data[0][emission_category.category.toLowerCase()]).toFixed(2)}`);
 
                             d3.select("#tooltip_answer_1")
                                 .text(`${data.data[1].name.split(",")[0]}: ${parseFloat(data.data[1][emission_category.category.toLowerCase()]).toFixed(2)}`);
+
+                            d3.select("#tooltip")
+                                .select("h2")
+                                .text(`${emission_category.danish_category}`);
 
                             d3.select("#tooltip")
                                 .select("p")
@@ -212,6 +197,46 @@ function get_answer() {
                                 .style("cursor", "pointer");
 
                             d3.select("#tooltip").classed("hidden", false);
+
+                            let xPosition = 0;
+                            let yPosition = 0;
+
+                            if (!window.matchMedia("(max-width: 793px)").matches) {
+                                if (parseInt(emission_category.category_id) < 4) {
+                                    xPosition = iconElement.x + iconElement.width;
+                                } else {
+                                    xPosition = iconElement.x - (45 * window.innerWidth / 100) - 5;
+                                }
+                                yPosition = document.getElementById(emission_category.danish_category.toLowerCase() + "_icon").getBoundingClientRect().bottom - document.getElementById("tooltip").getBoundingClientRect().height;
+                            }
+                            else {
+                                xPosition = document.getElementById("emission_breakdown_svg").getBoundingClientRect().left;
+                                yPosition = document.getElementById("emission_breakdown_svg").getBoundingClientRect().top - document.getElementById("tooltip").getBoundingClientRect().height - 5;
+                            }
+                            d3.select("#tooltip")
+                                .style("left", xPosition + "px")
+                                .style("top", yPosition + "px")
+
+                            svg.selectAll('rect').
+                                style("opacity", function () {
+                                    if (this.id != barId) {
+                                        return 50 + "%";
+                                    }
+                                    else {
+                                        return 100 + "%";
+                                    }
+                                });
+
+                            d3.select("#emission_category_container").selectAll("div")
+                                .style("opacity", function () {
+                                    if (this.id != emission_category.danish_category.toLowerCase() + "_icon") {
+                                        return 50 + "%";
+                                    }
+                                    else {
+                                        return 100 + "%";
+                                    }
+                                });
+
                         })
                         .on("mouseout", function () {
                             d3.select("#tooltip").classed("hidden", true);
@@ -219,36 +244,29 @@ function get_answer() {
                             d3.select("#" + emission_category.danish_category.toLowerCase() + "_icon")
                                 .style("transform", "scale(1.0)")
                                 .style("cursor", "default");
+
+                            svg.selectAll('rect').
+                                style("opacity", "100%");
+
+                            d3.select("#emission_category_container").selectAll("div")
+                                .style("opacity", "100%");
                         });
 
                     d3.select(`#${emission_category.danish_category.toLowerCase()}_icon`)
                         .append("img")
                         .attr("src", `images/icon_${emission_category.danish_category.toLowerCase()}.png`)
                         .on("mouseover", function () {
-
                             let iconElement = document.getElementById(emission_category.danish_category.toLowerCase() + "_icon").getBoundingClientRect();
-
-                            let xPosition = 0;
-
-                            if (parseInt(emission_category.category_id) < 4) {
-                                xPosition = iconElement.x + iconElement.width;
-                            } else {
-                                xPosition = iconElement.x - (45 * window.innerWidth / 100) - 5;
-                            }
-
-                            let yPosition = document.getElementById("emission_breakdown").getBoundingClientRect().top;
-
-                            d3.select("#tooltip")
-                                .style("left", xPosition + "px")
-                                .style("top", yPosition + "px")
-                                .select("h2")
-                                .text(`${emission_category.danish_category}`);
 
                             d3.select("#tooltip_answer_0")
                                 .text(`${data.data[0].name.split(",")[0]}: ${parseFloat(data.data[0][emission_category.category.toLowerCase()]).toFixed(2)}`);
 
                             d3.select("#tooltip_answer_1")
                                 .text(`${data.data[1].name.split(",")[0]}: ${parseFloat(data.data[1][emission_category.category.toLowerCase()]).toFixed(2)}`);
+
+                            d3.select("#tooltip")
+                                .select("h2")
+                                .text(`${emission_category.danish_category}`);
 
                             d3.select("#tooltip")
                                 .select("p")
@@ -259,6 +277,46 @@ function get_answer() {
                                 .style("cursor", "pointer");
 
                             d3.select("#tooltip").classed("hidden", false);
+
+                            let xPosition = 0;
+                            let yPosition = 0;
+
+                            if (!window.matchMedia("(max-width: 793px)").matches) {
+                                if (parseInt(emission_category.category_id) < 4) {
+                                    xPosition = iconElement.x + iconElement.width;
+                                } else {
+                                    xPosition = iconElement.x - (45 * window.innerWidth / 100) - 5;
+                                }
+                                yPosition = document.getElementById(emission_category.danish_category.toLowerCase() + "_icon").getBoundingClientRect().bottom - document.getElementById("tooltip").getBoundingClientRect().height;
+                            }
+                            else {
+                                xPosition = document.getElementById("emission_breakdown_svg").getBoundingClientRect().left;
+                                yPosition = document.getElementById("emission_breakdown_svg").getBoundingClientRect().top - document.getElementById("tooltip").getBoundingClientRect().height - 5;
+                            }
+                            d3.select("#tooltip")
+                                .style("left", xPosition + "px")
+                                .style("top", yPosition + "px")
+
+                            svg.selectAll('rect').
+                                style("opacity", function () {
+                                    if (this.id != barId) {
+                                        return 50 + "%";
+                                    }
+                                    else {
+                                        return 100 + "%";
+                                    }
+                                });
+
+                            d3.select("#emission_category_container").selectAll("div")
+                                .style("opacity", function () {
+                                    if (this.id != emission_category.danish_category.toLowerCase() + "_icon") {
+                                        return 50 + "%";
+                                    }
+                                    else {
+                                        return 100 + "%";
+                                    }
+                                });
+
                         })
                         .on("mouseout", function () {
                             d3.select("#tooltip").classed("hidden", true);
@@ -266,6 +324,12 @@ function get_answer() {
                             d3.select("#" + emission_category.danish_category.toLowerCase() + "_icon")
                                 .style("transform", "scale(1.0)")
                                 .style("cursor", "default");
+
+                            svg.selectAll('rect').
+                                style("opacity", "100%");
+
+                            d3.select("#emission_category_container").selectAll("div")
+                                .style("opacity", "100%");
                         });
                 });
             });
--- public/scripts/general.js
+++ public/scripts/general.js
@@ -1,34 +1,5 @@
-function test() {
-    console.log("test");
-}
-
-//Simple log check for localQuestionCounter.
+//Simpel log check for localQuestionCounter.
 function check_question_id() {
     console.log("localQuestionId: " + localStorage.getItem('localQuestionId'));
     console.log("localAnswers: " + localStorage.getItem('localAnswers'));
 }
-
-function show_infobox(event) {
-
-    console.log("Event: " + event);
-
-    // Læs søjlens x og y position ud fra 'this'
-    // Husk parseFloat for at lave text til number.
-    const xPosition = parseFloat(d3.select(this).attr("x"));
-    const yPosition = parseFloat(d3.select(this).attr("y")) + 10;
-
-    // Flyt tooltip div til rigtig position
-    d3.select("#tooltip")
-        .style("left", xPosition + "px")
-        .style("top", yPosition + "px")
-        .select("#value")
-        .text("test");
-
-    // Vis tooltip på ny position
-    d3.select("#tooltip").classed("hidden", false);
-}
-
-function hide_infobox() {
-    d3.select("#tooltip").classed("hidden", true);
-}
-
